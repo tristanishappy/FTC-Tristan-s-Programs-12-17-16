cc337c35be4b585a29a27a18ead69b26fa489f7b
Tristan <tristanmentink@hotmail.com>
2017-03-04 18:09:29 -0800

    Add files via upload

--- AutonomousBlueButtonAaron.java
+++ AutonomousBlueButtonAaron.java
@@ -11,14 +11,15 @@ import com.qualcomm.robotcore.hardware.DeviceInterfaceModule;
 import com.qualcomm.robotcore.hardware.I2cDevice;
 import com.qualcomm.robotcore.hardware.LightSensor;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.UltrasonicSensor;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.I2cController;
+import com.qualcomm.robotcore.hardware.UltrasonicSensor;
 import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.hardware.adafruit.AdafruitBNO055IMU;
 
 import org.firstinspires.ftc.robotcontroller.external.samples.ModernRoboticsI2cColorSensor2;
 
-public class AutonomousRedButtonAaron extends LinearOpMode
+public class AutonomousBlueButtonAaron extends LinearOpMode
 {
     //Declares name of DcMotors
     DcMotor motorRF;            //Gamepad 1: right stick and left stick
@@ -30,18 +31,14 @@ public class AutonomousRedButtonAaron extends LinearOpMode
 
     LightSensor light;
 
-    UltrasonicSensor eyes;
-
     I2cDevice buttonColor;
-
+    UltrasonicSensor eyes;
     DcMotor motorCol;
     DcMotor motorPop;
     private double lift = 0;
 
     Servo servoL;
     Servo servoR;
-
-
 
     @Override
     public void runOpMode() throws InterruptedException
@@ -57,11 +54,12 @@ public class AutonomousRedButtonAaron extends LinearOpMode
         motorRB = hardwareMap.dcMotor.get("motorRB");
         motorLB = hardwareMap.dcMotor.get("motorLB");
 
+        //buttonColor = hardwareMap.colorSensor.get("color");
+        eyes = hardwareMap.ultrasonicSensor.get("eyes");
+
         light = hardwareMap.lightSensor.get("light");
         motorCol = hardwareMap.dcMotor.get("collector");
         motorPop = hardwareMap.dcMotor.get("popper");
-
-        eyes = hardwareMap.ultrasonicSensor.get("eyes");
 
         motorCol.setDirection(DcMotor.Direction.REVERSE);
 
@@ -72,8 +70,11 @@ public class AutonomousRedButtonAaron extends LinearOpMode
         light.enableLed(true);
         //buttonColor.enableLed(false);
         //Movement
+
+
+
         motorPop.setPower(1);
-        sleep(1400);
+        sleep(1500);
         motorPop.setPower(0);
         sleep(100);
 
@@ -83,39 +84,93 @@ public class AutonomousRedButtonAaron extends LinearOpMode
         mecaMovement(0,0.5);
         sleep(500);
 
-        movement(-1,1);//turn left
-        sleep(710);
+        movement(1,-1);//turn right
+        sleep(660);
 
-        mecaMovement(90,0.4);
-        sleep(555);
+        mecaMovement(270,0.3);
+        sleep(850);
 
         mecaMovement(0,0.5);
         sleep(500);
 
-        mecaMovement(315, 0.5);
+        mecaMovement(45, 0.5);
         sleep(4000);
 
-        mecaMovement(0,0);//nah
-        sleep(300);
-
-        int time = 0;
-        mecaMovement(270, 0.35);
-        while (light.getLightDetected() <= 0.4 && time < 5000)
-        {
-            //telemetry.addData("Light  ", light.getLightDetected());
-            time++;
-            sleep(1);
-        }
-
-        mecaMovement(90,0.5);
+        mecaMovement(270,0.45);//nah
         sleep(400);
 
-        mecaMovement(0,0);//nah?
-        sleep(300);
+
+        mecaMovement(90, 0.35);
+        while (light.getLightDetected() <= 0.4)
+        {
+            //telemetry.addData("Light  ", light.getLightDetected());
+            sleep(1);
+        }
+        //mecaMovement(90,0.5);
+        //sleep(300);
+
+        mecaMovement(90,0.4);//nah?
+        sleep(120);
 
         telemetry.addData("Colornumber: ",colorx.colorNumber());
         telemetry.addData("Red: ",colorx.red());
         telemetry.addData("Blue: ",colorx.blue());
+
+        mecaMovement(0,0);
+        sleep(200);
+
+        int time = 0;
+        while(eyes.getUltrasonicLevel() > 27 || eyes.getUltrasonicLevel() == 0 || eyes.getUltrasonicLevel() == 255 && time <= 2000)
+        {
+            time += 1;
+            mecaMovement(0,0.5);
+            sleep(1);
+        }
+        mecaMovement(0,0);
+
+        mecaMovement(0,0);
+        sleep(300);
+
+        if(colorx.colorNumber() < 4 && colorx.colorNumber() != 0)
+        {
+            movement(-1,1);//Right
+            sleep(7);
+            mecaMovement(0,0.4);
+            sleep(1300);
+        }
+        else if (colorx.colorNumber() >= 4){
+            mecaMovement(90,0.4);
+            sleep(420);
+            movement(1,-1);//left
+            sleep(10);
+            mecaMovement(0,0.35);
+            sleep(1300);
+        }
+        mecaMovement(180, 0.35);
+        sleep(400);
+
+        mecaMovement(90, 0.4);
+        sleep(200);
+
+        mecaMovement(90,0.5);
+        sleep(1000);
+        mecaMovement(0,0.4);
+        sleep(1500);
+        mecaMovement(180, 0.5);
+        sleep(450);
+
+
+        mecaMovement(90, 0.35);
+        while (light.getLightDetected() <= 0.4)
+        {
+            //telemetry.addData("Light  ", light.getLightDetected());
+            sleep(1);
+        }
+        mecaMovement(90,0.35);
+        sleep(180);
+
+        mecaMovement(0,0);//nah?
+        sleep(300);
 
         time = 0;
         while(eyes.getUltrasonicLevel() > 27 || eyes.getUltrasonicLevel() == 0 || eyes.getUltrasonicLevel() == 255 && time <= 2000)
@@ -126,101 +181,55 @@ public class AutonomousRedButtonAaron extends LinearOpMode
         }
         mecaMovement(0,0);
 
-
-        if(colorx.colorNumber() >= 6)
+        if(colorx.colorNumber() < 4 && colorx.colorNumber() != 0)
         {
+            movement(-1,1);//Right
+            sleep(5);
             mecaMovement(0,0.35);
             sleep(1300);
             mecaMovement(180, 0.35);
-            sleep(400);
-        }
-        else if (colorx.colorNumber() < 6 && colorx.colorNumber() != 0){
-            mecaMovement(90,0.4);
-            sleep(370);
-            mecaMovement(0,0.35);
-            sleep(1300);
-            mecaMovement(180, 0.35);
-            sleep(400);
-
-        }
-
-        mecaMovement(270, 0.4);
-        sleep(200);
-
-        mecaMovement(270,0.8);
-        sleep(700);
-        mecaMovement(0,0.35);
-        sleep(1800);
-        mecaMovement(180, 0.4);
-        sleep(400);
-
-
-        mecaMovement(270, 0.35);
-        time = 0;
-        while (light.getLightDetected() <= 0.4 && time < 5000)
-        {
-            //telemetry.addData("Light  ", light.getLightDetected());
-            time++;
-            sleep(1);
-        }
-        mecaMovement(90,0.5);
-        sleep(400);
-
-        mecaMovement(0,0);//nah?
-        sleep(300);
-
-        time = 0;
-        while(eyes.getUltrasonicLevel() > 27 || eyes.getUltrasonicLevel() == 0 || eyes.getUltrasonicLevel() == 255 && time <= 2000)
-        {
-            time += 1;
-            mecaMovement(90,0.5);
-            sleep(1);
-        }
-        telemetry.addData("Colornumber: ",colorx.colorNumber());
-        if(colorx.colorNumber() >= 6)
-        {
-            mecaMovement(0,0.35);
-            sleep(1300);
-            mecaMovement(180, 0.35);
-            sleep(620);
+            sleep(600);
             mecaMovement(90,0.35);
             sleep(250);
         }
-        else if(colorx.colorNumber() < 6 && colorx.colorNumber() != 0){
-            mecaMovement(90,0.4);
-            sleep(370);
+        else if(colorx.colorNumber() >= 4){
+            mecaMovement(90,0.35);
+            sleep(300);
             mecaMovement(0,0.35);
             sleep(1300);
             mecaMovement(180, 0.35);
-            sleep(620);
-            movement(-1,1);//turn left
-            sleep(70);
+            sleep(400);
 
         }
+        else
+        {
+            mecaMovement(180, 0.35);
+            sleep(400);
+        }
+        mecaMovement(180,0);
+        sleep(300);
+
+        movement(1,-1);//turn right
+        sleep(1035);
+
+        mecaMovement(0,0.6);
+        sleep(900);
         mecaMovement(0,0);
-        sleep(10);
+        sleep(100);
 
-        movement(-1,1);//turn left
-        sleep(1095);
-
-        mecaMovement(0,1);
-        sleep(800);
-
-        mecaMovement(0, 1);
-        motorCol.setPower(0.75);
-        sleep(400);
-        mecaMovement(0, 0);
-        motorCol.setPower(0.75);
-        sleep(1400);
+        motorCol.setPower(0.7);
+        sleep(1500);
         motorCol.setPower(0);
         motorPop.setPower(1);
-        sleep(1300);
+        sleep(1500);
         motorPop.setPower(0);
+        sleep(100);
 
         mecaMovement(0, 1);
-        sleep(2500);
-        //Until End
+        sleep(2650);
 
+        mecaMovement(0, 0);
+        sleep(10);
         telemetry.addLine("Did it work???");
     }
 
